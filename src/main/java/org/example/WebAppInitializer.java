e04576b4c44ba021915467c2b31c1df7080a52c3
nikitaln <nikitaln7@gmail.com>
2023-10-20 16:43:34 +0300

    database working

--- src/main/java/org/example/WebAppInitializer.java
+++ src/main/java/org/example/WebAppInitializer.java
@@ -3,10 +3,10 @@ package org.example;
 import org.apache.log4j.Logger;
 import org.example.app.config.AppContextConfig;
 import org.example.web.config.WebContextConfig;
+import org.h2.server.web.WebServlet;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
@@ -39,5 +39,9 @@ public class WebAppInitializer implements WebApplicationInitializer {
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
         logger.info("dispatcher ready");
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("h2-console", new WebServlet());
+        servlet.setLoadOnStartup(2);
+        servlet.addMapping("/console/*");
     }
 }
---
+++ src/main/java/org/example/app/config/DbConfig.java
@@ -0,0 +1,25 @@
+package org.example.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(false)
+                .setName("book_storage")
+                .setType(EmbeddedDatabaseType.H2)
+                .addDefaultScripts()
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .build();
+    }
+
+}
